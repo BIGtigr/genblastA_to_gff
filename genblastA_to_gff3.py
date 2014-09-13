d3b589bfa66ab90a77e237ffc636d9b2a9eb9f06
Peter van Heusden <pvh@sanbi.ac.za>
2014-09-13 19:32:17 +0200

    * Update genomic_match regexp to support - in score (negative score)

--- genblastA_to_gff3.py
+++ genblastA_to_gff3.py
@@ -8,7 +8,67 @@ import logging.config
 import json
 import os
 
-def write_gff(genomic_match, hsp_dict, query_name, output_file):
+START_STR = '//*****************START'
+END_STR = '//******************END'
+QUERY_NAME_STR = '//for query: '
+HSP_STR = 'HSP_ID'
+GENOMIC_MATCH_RE = re.compile('^(?P<query_name>[^|]*)\|(?P<match_name>[^:]*):(?P<match_start>\d+)\.\.(?P<match_end>\d+)\|(?P<strand>[+-])\|gene cover:(?P<coverage_num>\d+)\((?P<coverage_perc>[\d.]+)%\)\|score:(?P<score>[-\d.]+)\|rank:(?P<rank>\d+)$')
+HSP_RE = re.compile('^HSP_ID\[(?P<hsp_id>\d+)\]:\((?P<match_start>\d+)-(?P<match_end>\d+)\);query:\((?P<query_start>\d+)-(?P<query_end>\d+)\); pid: (?P<perc_id>[\d.]+)$')
+
+def parse_genblastA(input_filename):
+	in_record = False
+	# variables set during parsing
+	hsp_dict = dict()
+	genomic_match = None
+	query_name = ''
+	for line in input_filename:
+		if not in_record:
+			if line.startswith(START_STR):
+				in_record = True
+		else:
+			# we're in a record
+			if line.startswith(END_STR):
+				# check that we've got a match to output (we might have NONE)
+				if genomic_match:
+					yield(dict(match=genomic_match, hsps=hsp_dict))
+				hsp_dict = dict()
+				genomic_match = None
+				query_name = ''
+				in_record = False
+			elif line.startswith(QUERY_NAME_STR):
+				fields = line.split()
+				if len(fields) != 4:
+					logging.error('Got wrong number of fields ({} vs 4) in line: {}'.format(len(fields), line))
+					in_record = False
+				else:
+					query_name = fields[2]
+			elif 'gene cover' in line:
+				if genomic_match:
+					# we've already seen one match, need to output that
+					yield(dict(match=genomic_match, hsps=hsp_dict))
+					# and reset the hsp_dict, we're about to reset the genomic_match
+					hsp_dict = dict()
+				genomic_match = GENOMIC_MATCH_RE.match(line.rstrip())
+				if not genomic_match:
+					logging.error('Genomic match regexp failed to match on line: {}'.format(line))
+					in_record = False
+				else:
+					# not much to do: we've got a genomic match saved in genomic_match, will use it once we've read the HSPs
+					logging.debug('Got match between {} and {} start: {} end: {}'.format(genomic_match.group('query_name'), 
+								  genomic_match.group('match_name'), genomic_match.group('match_start'), genomic_match.group('match_end')))
+			elif line.startswith(HSP_STR):
+				match = HSP_RE.match(line.rstrip())
+				if not match:
+					logging.error('HSP regexp failed to match on line: {}'.format(line))
+					in_record = False
+				else:
+					# save the HSPs for this genomic match
+					hsp = dict(match_start=int(match.group('match_start')), match_end=int(match.group('match_end')), 
+						       query_start=int(match.group('query_start')), query_end=int(match.group('query_end')), 
+						       perc_id=float(match.group('perc_id')))
+					hsp_dict[int(match.group('hsp_id'))] = hsp
+
+def write_gff_line(genomic_match, hsp_dict, query_name, output_file):
 	# gff3 format
 	# seq source type start end score strand phase attributes"
 	num_hsps = len(hsp_dict)
@@ -49,61 +109,5 @@ parser.add_argument('gff_file', nargs='?', type=argparse.FileType('w'), default=
 args = parser.parse_args()
 
 args.gff_file.write('##gff-version 3\n')
-start_str = '//*****************START'
-end_str = '//******************END'
-query_name_str = '//for query: '
-hsp_str = 'HSP_ID'
-genomic_match_re = re.compile('^(?P<query_name>[^|]*)\|(?P<match_name>[^:]*):(?P<match_start>\d+)\.\.(?P<match_end>\d+)\|(?P<strand>[+-])\|gene cover:(?P<coverage_num>\d+)\((?P<coverage_perc>[\d.]+)%\)\|score:(?P<score>[\d.]+)\|rank:(?P<rank>\d+)$')
-hsp_re = re.compile('^HSP_ID\[(?P<hsp_id>\d+)\]:\((?P<match_start>\d+)-(?P<match_end>\d+)\);query:\((?P<query_start>\d+)-(?P<query_end>\d+)\); pid: (?P<perc_id>[\d.]+)$')
-in_record = False
-# variables set during parsing
-hsp_dict = dict()
-genomic_match = None
-query_name = ''
-for line in args.genblastA_file:
-	if not in_record:
-		if line.startswith(start_str):
-			in_record = True
-	else:
-		# we're in a record
-		if line.startswith(end_str):
-			# check that we've got a match to output (we might have NONE)
-			if genomic_match:
-				write_gff(genomic_match, hsp_dict, query_name, args.gff_file)
-			hsp_dict = dict()
-			genomic_match = None
-			query_name = ''
-			in_record = False
-		elif line.startswith(query_name_str):
-			fields = line.split()
-			if len(fields) != 4:
-				logging.error('Got wrong number of fields ({} vs 4) in line: {}'.format(len(fields), line))
-				in_record = False
-			else:
-				query_name = fields[2]
-		elif 'gene cover' in line:
-			if genomic_match:
-				# we've already seen one match, need to output that
-				write_gff(genomic_match, hsp_dict, query_name, args.gff_file)
-				# and reset the hsp_dict, we're about to reset the genomic_match
-				hsp_dict = dict()
-			genomic_match = genomic_match_re.match(line.rstrip())
-			if not genomic_match:
-				logging.error('Genomic match regexp failed to match on line: {}'.format(line))
-				in_record = False
-			else:
-				# not much to do: we've got a genomic match saved in genomic_match, will use it once we've read the HSPs
-				logging.debug('Got match between {} and {} start: {} end: {}'.format(genomic_match.group('query_name'), 
-							  genomic_match.group('match_name'), genomic_match.group('match_start'), genomic_match.group('match_end')))
-		elif line.startswith(hsp_str):
-			match = hsp_re.match(line.rstrip())
-			if not match:
-				logging.error('HSP regexp failed to match on line: {}'.format(line))
-				in_record = False
-			else:
-				# save the HSPs for this genomic match
-				hsp = dict(match_start=int(match.group('match_start')), match_end=int(match.group('match_end')), 
-					       query_start=int(match.group('query_start')), query_end=int(match.group('query_end')), 
-					       perc_id=float(match.group('perc_id')))
-				hsp_dict[int(match.group('hsp_id'))] = hsp
-
+for match in parse_genblastA(args.genblastA_file):
+	write_gff_line(match['match'], match['hsps'], match['match'].group('query_name'), args.gff_file)
