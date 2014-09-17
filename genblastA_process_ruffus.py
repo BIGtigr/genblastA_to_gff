6b6038f5200ca6e5dd3ee3f890ee805607bfdeea
Peter van Heusden <pvh@sanbi.ac.za>
2014-09-17 22:22:45 +0200

    * Add support to generate SeqIO on-disk index of FASTA file

--- genblastA_process_ruffus.py
+++ genblastA_process_ruffus.py
@@ -8,6 +8,7 @@ import glob
 from subprocess import Popen, PIPE
 import shlex
 
+from Bio import SeqIO
 from ruffus import *
 from genblastA_to_gff3 import genblastA_process
 # import Environment Modules stuff
@@ -23,7 +24,10 @@ parser.add_argument('--input_pattern', '-I', default='.out')
 parser.add_argument('--working_directory', '-W', default='.')
 parser.add_argument('--num_threads', '-N', type=int, default=1)
 parser.add_argument('genome_filename', help='FASTA format genome file, must end in .fa or .fasta')
+parser.add_argument('query_filename', help='FASTA format file of query sequences, must end in .fa or .fasta')
 args = parser.parse_args()
+
+FASTA_RE=r'\.(fa|fasta)$'
 
 os.chdir(args.working_directory)
 starting_files = glob.glob('*' + args.input_pattern)
@@ -45,7 +49,7 @@ def genblastA_to_gff3(input_file, output_file):
 	genblastA_process(in_file, out_file, min_perc_coverage=80.0)
 
 @transform(args.genome_filename,
-	       regex('\.(fa|fasta)$'),
+	       regex(FASTA_RE),
 	       '.2bit')
 def make_twobit(input_file, output_file):
 	cmdline = 'faToTwoBit -noMask {} {}'.format(input_file, output_file)
@@ -56,4 +60,11 @@ def make_twobit(input_file, output_file):
 	if (retcode != 0):
 		sys.stderr.write('Failed to run faToTwoBit (cmdline: {}): output:\n{}\nerror:{}\n'.format(cmdline, output, error))
 
+@transform(args.query_filename,
+	       regex(FASTA_RE),
+		   '.idx')
+def make_index(input_file, output_file):
+	SeqIO.index_db(output_file, input_file, 'fasta')
+
+def run_exonerate(input_files, output_file, )
 pipeline_run(multiprocess=args.num_threads)
